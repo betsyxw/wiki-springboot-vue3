1d4c2408d0accfbb1e1a25777485c66befe70c8c
betsyxw <betsywenwen@yahoo.co.jp>
2021-04-29 19:24:58 +0800

    2-2 init

---
+++ src/test/java/com/xuwen/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuwen.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

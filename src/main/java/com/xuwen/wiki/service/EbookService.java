17e472cfc2efb0c51af84adcd56f2693ffacc248
betsywenwen@yahoo.co.jp <betsywenwen@yahoo.co.jp>
2021-08-14 19:17:28 +0800

    3-8CopyUtil工具类

--- src/main/java/com/xuwen/wiki/service/EbookService.java
+++ src/main/java/com/xuwen/wiki/service/EbookService.java
@@ -2,14 +2,13 @@ package com.xuwen.wiki.service;
 
 import com.xuwen.req.EbookReq;
 import com.xuwen.resp.EbookResp;
+import com.xuwen.util.CopyUtil;
 import com.xuwen.wiki.domain.Ebook;
 import com.xuwen.wiki.domain.EbookExample;
 import com.xuwen.wiki.mapper.EbookMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,13 +26,18 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebooksList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebooksList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+        //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+        List<EbookResp> list = CopyUtil.copyList(ebooksList, EbookResp.class);
+        return list;
     }
 
 }

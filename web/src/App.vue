130dc64ce5a31183a53decbc87104fdd593eb54f
betsywenwen@yahoo.co.jp <betsywenwen@yahoo.co.jp>
2021-08-14 22:14:53 +0800

    4-7,组件拆分，the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,7 +9,7 @@
 <script lang="ts">
   import { defineComponent } from 'vue';
   import TheHeader from "@/components/the-header.vue";
-  import TheFooter from "@/components/the-footer";
+  import TheFooter from "@/components/the-footer.vue";
 
   export default defineComponent({
     name: 'app',
